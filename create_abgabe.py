d61d31e1d59ed7c65c3d080bb43f25339854c15d
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2025-07-16 10:15:58 +0200

    mesh almost done

---
+++ create_abgabe.py
@@ -0,0 +1,16 @@
+
+import os
+
+task = "a"
+subnumber = "1"
+
+foldername = "AltKlausuren/SS 25"
+
+filename = foldername + "Hoegen_82358_Teil" + task + ".py"
+
+functions_filename = "functions.py"
+
+save_path = "Hoegen_82358"
+os.mkdir(save_path, exist_ok=True)
+os.chdir(save_path)
+os.write()
